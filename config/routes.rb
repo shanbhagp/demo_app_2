a0584d2fa965009c25c1e3663e5b4714117e40c8
Praveen R. Shanbhag <shanbhagp@aol.com>
2012-07-02 02:40:03 -0700

    2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 DemoApp2::Application.routes.draw do
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
+   match '/users' => 'users#index'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
@@ -48,7 +50,7 @@ DemoApp2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
